87b3808a3b9371084a458ae7af345c4c9ba9611d
VGMTAServer <VGMTAServer@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-09-26 07:26:55 +0000

    /showcol
    git-svn-id: https://mabako.net:8081@28 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/realism-system/c_headbob.lua
+++ src_trunk/resources/realism-system/c_headbob.lua
@@ -16,3 +16,50 @@ function resetCam()
 	setCameraTarget(getLocalPlayer())
 end
 addCommandHandler("resetcam", resetCam)
+
+local show = false
+function showColBox()
+	if (show) then
+		removeEventHandler("onClientPreRender", getRootElement(), drawBox)
+		show = false
+	else
+		addEventHandler("onClientPreRender", getRootElement(), drawBox)
+		show = true
+	end
+end
+addCommandHandler("showcol", showColBox)
+
+function drawBox()
+	for key, value in ipairs(getElementsByType("player")) do
+		if isElementStreamedIn(value) and (isElementOnScreen(value)) then
+			if (value~=getLocalPlayer()) then
+				local px, py, pz = getElementPosition(value)
+				local x, y, z, x2, y2, z2 = getElementBoundingBox(value)
+					
+				local width = 4
+				dxDrawLine3D(px+x, py+y, pz+z, px+x, py+y, pz+z2, tocolor(255, 0, 0, 150), width, true, 0)
+				dxDrawLine3D(px+x, py+y2, pz+z2, px+x, py+y2, pz+z, tocolor(255, 0, 0, 150), width, true, 1)
+				dxDrawLine3D(px+x2, py+y, pz+z2, px+x2, py+y, pz+z, tocolor(255, 0, 0, 150), width, true, 2)
+				dxDrawLine3D(px+x2, py+y2, pz+z2, px+x2, py+y2, pz+z, tocolor(255, 0, 0, 150), width, true, 3)
+					
+				dxDrawLine3D(px+x, py+y2, pz+z2, px+x2, py+y2, pz+z2, tocolor(255, 0, 0, 150), width, true, 4)
+				dxDrawLine3D(px+x2, py+y2, pz+z2, px+x2, py+y, pz+z2, tocolor(255, 0, 0, 150), width, true, 5)
+				dxDrawLine3D(px+x, py+y, pz+z2, px+x, py+y2, pz+z2, tocolor(255, 0, 0, 150), width, true, 6)
+				dxDrawLine3D(px+x, py+y, pz+z2, px+x2, py+y, pz+z2, tocolor(255, 0, 0, 150), width, true, 7)
+					
+				dxDrawLine3D(px+x, py+y2, pz+z2, px+x2, py+y2, pz+z, tocolor(255, 0, 0, 150), width, true, 8)
+				dxDrawLine3D(px+x2, py+y2, pz+z2, px+x2, py+y, pz+z, tocolor(255, 0, 0, 150), width, true, 9)
+				dxDrawLine3D(px+x, py+y, pz+z2, px+x, py+y2, pz+z, tocolor(255, 0, 0, 150), width, true, 10)
+				dxDrawLine3D(px+x, py+y, pz+z2, px+x2, py+y, pz+z, tocolor(255, 0, 0, 150), width, true, 11)
+					
+				dxDrawLine3D(px+x, py+y2, pz+z, px+x2, py+y2, pz+z, tocolor(255, 0, 0, 150), width, true, 8)
+				dxDrawLine3D(px+x2, py+y2, pz+z, px+x2, py+y, pz+z, tocolor(255, 0, 0, 150), width, true, 9)
+				dxDrawLine3D(px+x, py+y, pz+z, px+x, py+y2, pz+z, tocolor(255, 0, 0, 150), width, true, 10)
+				dxDrawLine3D(px+x, py+y, pz+z, px+x2, py+y, pz+z, tocolor(255, 0, 0, 150), width, true, 11)
+					
+				dxDrawLine3D(px+x, py+y2, pz+z2, px+x2, py+y, pz+z2, tocolor(255, 0, 0, 150), width, true, 12)
+				dxDrawLine3D(px+x, py+y2, pz+z, px+x2, py+y, pz+z, tocolor(255, 0, 0, 150), width, true, 12)
+			end
+		end
+	end
+end
