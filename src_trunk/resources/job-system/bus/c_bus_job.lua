1a8b002514e572250a1e5199da77735e4ee51ae4
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-09-26 02:20:34 +0000

    fixed a warning for leaving players not in a vehicle
    git-svn-id: https://mabako.net:8081@8 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/job-system/bus/c_bus_job.lua
+++ src_trunk/resources/job-system/bus/c_bus_job.lua
@@ -294,7 +294,7 @@ addEventHandler("onClientVehicleStartEnter", getRootElement(), startEnterBus)
 function onPlayerQuit()
 	if getElementData(source, "job") == 3 then
 		vehicle = getPedOccupiedVehicle(source)
-		if bus[getElementModel(vehicle)] and getVehicleOccupant(vehicle) == source then
+		if vehicle and bus[getElementModel(vehicle)] and getVehicleOccupant(vehicle) == source then
 			setVehicleLocked(vehicle, false)
 		end
 	end
