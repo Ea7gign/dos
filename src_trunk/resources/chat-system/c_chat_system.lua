0fbe9297d3740c5b3c9aa891f2c24e99c8105b1d
VGMTAServer <VGMTAServer@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-09-27 10:21:50 +0000

    Added a small debug cmd
    git-svn-id: https://mabako.net:8081@46 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/chat-system/c_chat_system.lua
+++ src_trunk/resources/chat-system/c_chat_system.lua
@@ -1,2 +1,23 @@
 bindKey("u", "down", "chatbox", "GlobalOOC")
 bindKey("b", "down", "chatbox", "LocalOOC")
+
+data = { }
+function datachange(value)
+	if (data[value] == nil) then
+		data[value] = 1
+	else
+		data[value] = data[value] + 1
+	end
+end
+addEventHandler("onClientElementDataChange", getRootElement(), datachange)
+
+local tick = getTickCount()
+function showdata()
+	local tick2 = getTickCount()
+	local time = (tick2 - tick) / 1000
+	outputChatBox("Sync for " .. time .. " seconds.")
+	for key, value in pairs(data) do
+		outputChatBox(tostring(key) .. ": ".. value)
+	end
+end
+addCommandHandler("showdata", showdata)
