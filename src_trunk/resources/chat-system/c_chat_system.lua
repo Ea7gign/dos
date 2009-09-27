69dbb96203dadf44bc2052929c12e2c0abdec36d
VGMTAServer <VGMTAServer@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-09-27 10:29:44 +0000

    Few debug command changes
    git-svn-id: https://mabako.net:8081@48 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/chat-system/c_chat_system.lua
+++ src_trunk/resources/chat-system/c_chat_system.lua
@@ -16,8 +16,17 @@ function showdata()
 	local tick2 = getTickCount()
 	local time = (tick2 - tick) / 1000
 	outputChatBox("Sync for " .. time .. " seconds.")
+	
+	local largest = 0
+	local largestname = "none"
 	for key, value in pairs(data) do
+		if (value>largest) then
+			largest = value
+			largestname = key
+		end
 		outputChatBox(tostring(key) .. ": ".. value)
 	end
+	
+	outputChatBox("Largest was: ".. largestname .. ": " .. largest)
 end
 addCommandHandler("showdata", showdata)
