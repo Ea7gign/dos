fb95bf872fe023460ee485d0057c92ec5ea965cf
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-09-28 18:57:09 +0000

    reverted r99: [SANN] Money from photo's now go to the faction bank
    git-svn-id: https://mabako.net:8081@101 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/job-system/photographer/s_photographer_job.lua
+++ src_trunk/resources/job-system/photographer/s_photographer_job.lua
@@ -1,15 +1,10 @@
 function getPaid(collectionValue)
-	
+	exports.global:giveMoney(getPlayerTeam(source), tonumber(collectionValue))
 	exports.global:sendLocalMeAction(source,"hands his collection of photographs to the woman behind the desk.")
 	exports.global:sendLocalText(source, "Victoria Greene says: Thank you. These should make the morning edition. Keep up the good work.", nil, nil, nil, 10)
-	--exports.global:giveMoney(getPlayerTeam(source), tonumber(collectionValue))	
-	--outputChatBox("#FF9933SAN News made $".. collectionValue .." from the photographs.", source, 255, 104, 91, true)
-	--exports.global:sendMessageToAdmins("SANNews: " .. tostring(getPlayerName(source)) .. " sold photos for $" .. collectionValue .. ".")
-	--exports.logs:logMessage(tostring(getPlayerName(source)) .. " sold photos for $" .. collectionValue .. ".", 10)
-
-	local newsCompany = getTeamFromName("San Andreas Network News")
-	local dbid = getElementData(newsCompany, "id")
-	call(getResourceFromName("faction-system"), "addToFactionMoney", dbid, collectionValue)
+	outputChatBox("#FF9933SAN News made $".. collectionValue .." from the photographs.", source, 255, 104, 91, true)
+	exports.global:sendMessageToAdmins("SANNews: " .. tostring(getPlayerName(source)) .. " sold photos for $" .. collectionValue .. ".")
+	exports.logs:logMessage(tostring(getPlayerName(source)) .. " sold photos for $" .. collectionValue .. ".", 10)
 end
 addEvent("submitCollection", true)
 addEventHandler("submitCollection", getRootElement(), getPaid)
