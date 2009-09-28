ac77cb1f5817cd9e353bf3a7ce4a04cebd937282
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-09-28 17:05:24 +0000

    fix for tazer
    git-svn-id: https://mabako.net:8081@96 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/lvpd-system/s_policedeagle.lua
+++ src_trunk/resources/lvpd-system/s_policedeagle.lua
@@ -3,13 +3,13 @@ function tazerFired(x, y, z, target)
 	local distance = getDistanceBetweenPoints3D(x, y, z, px, py, pz)
 
 	if (distance<50) then
-		for key, value in ipairs(exports.global:getNearbyElements(target, "player", 50)) do
-			if (value~=source) then
-				triggerClientEvent(value, "showTazerEffect", value, x, y, z) -- show the sparks
-			end
-		end
-		
 		if (isElement(target) and getElementType(target)=="player") then
+			for key, value in ipairs(exports.global:getNearbyElements(target, "player", 50)) do
+				if (value~=source) then
+					triggerClientEvent(value, "showTazerEffect", value, x, y, z) -- show the sparks
+				end
+			end
+			
 			setElementData(target, "tazed", 1, false)
 			toggleAllControls(target, false, true, false)
 			exports.global:applyAnimation(target, "ped", "FLOOR_hit_f", -1, false, false, true)
