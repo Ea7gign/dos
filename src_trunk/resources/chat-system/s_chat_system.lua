08dc46a0bdff40df05e6d991dbe7e7140f75de00
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-10-03 20:38:46 +0000

    some update to pm
    git-svn-id: https://mabako.net:8081@153 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/chat-system/s_chat_system.lua
+++ src_trunk/resources/chat-system/s_chat_system.lua
@@ -470,17 +470,21 @@ function pmPlayer(thePlayer, commandName, who, ...)
 								break
 							end
 						end
+					end
+					
+					-- Send the message
+					local playerid = getElementData(thePlayer, "playerid")
+					local targetid = getElementData(targetPlayer, "playerid")
+					outputChatBox("PM From (" .. playerid .. ") " .. playerName .. ": " .. message, targetPlayer, 255, 255, 0)
+					outputChatBox("PM Sent to (" .. targetid .. ") " .. targetPlayerName .. ": " .. message, thePlayer, 255, 255, 0)
 						
-						-- Send the message
-						local playerid = getElementData(thePlayer, "playerid")
-						local targetid = getElementData(targetPlayer, "playerid")
-						outputChatBox("PM From (" .. playerid .. ") " .. playerName .. ": " .. message, targetPlayer, 255, 255, 0)
-						outputChatBox("PM Sent to (" .. targetid .. ") " .. targetPlayerName .. ": " .. message, thePlayer, 255, 255, 0)
-						
+					if not exports.global:isPlayerScripter(thePlayer) and not exports.global:isPlayerScripter(targetPlayer) then
 						exports.logs:logMessage("[PM From " ..playerName .. " TO " .. targetPlayerName .. "]" .. message, 8)
 						
 						-- big ears
 						local received = {}
+						received[thePlayer] = true
+						received[targetPlayer] = true
 						for key, value in pairs( getElementsByType( "player" ) ) do
 							if isElement( value ) and not received[value] then
 								local listening = getElementData( value, "bigears" )
