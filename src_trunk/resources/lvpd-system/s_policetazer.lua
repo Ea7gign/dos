22757d7d2a48ceed8ef43debba4f63b371816230
Fenix <Fenix@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-10-04 16:29:33 +0000

    Small bandwidth optimization to switching weapons
    git-svn-id: https://mabako.net:8081@168 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/lvpd-system/s_policetazer.lua
+++ src_trunk/resources/lvpd-system/s_policetazer.lua
@@ -1,7 +1,9 @@
 function sendWeaponSwitchToAll(oldSlot, newSlot)
-	for key, value in ipairs(getElementsByType("player")) do
-		if (value ~= source) then
-			triggerClientEvent(value, "onPlayerWeaponSwitch", source, oldSlot, newSlot)
+	if (oldSlot == 2 or newSlot == 2 or oldSlot == 5 or newslot == 5) then
+		for key, value in ipairs(getElementsByType("player")) do
+			if (value ~= source) then
+				triggerClientEvent(value, "onPlayerWeaponSwitch", source, oldSlot, newSlot)
+			end
 		end
 	end
 end
