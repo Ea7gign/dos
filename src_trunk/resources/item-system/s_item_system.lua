15171cae779ad277ab3ecd2db187ed515cfac356
Mount <Mount@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-10-04 09:49:00 +0000

    fixed vehicle start exploit.
    git-svn-id: https://mabako.net:8081@161 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/item-system/s_item_system.lua
+++ src_trunk/resources/item-system/s_item_system.lua
@@ -177,9 +177,14 @@ function useItem(itemSlot, additional)
 			
 			local skin = tonumber(itemValue)
 			if fittingskins[gender] and fittingskins[gender][race] and fittingskins[gender][race][skin] then
+				local vehicle = getPedOccupiedVehicle ( source )
+				local veholdstate = getVehicleEngineState ( vehicle )		
 				setPedSkin(source, skin)
 				setElementData(source, "casualskin", skin, false)
 				exports.global:sendLocalMeAction(source, "changes their clothes.")
+				if (vehicle) then
+					setVehicleEngineState ( vehicle, veholdstate )
+				end
 			else
 				outputChatBox("These clothes do not fit you.", source, 255, 0, 0)
 			end
