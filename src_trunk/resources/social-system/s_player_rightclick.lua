1b943382386d94bf4335fc0e5f16008a5fdbd920
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-10-11 15:55:52 +0000

    'information' fix
    git-svn-id: https://mabako.net:8081@263 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/social-system/s_player_rightclick.lua
+++ src_trunk/resources/social-system/s_player_rightclick.lua
@@ -11,7 +11,7 @@ function retrievePlayerInfo(targetPlayer)
 			end
 			mysql_free_result( result )
 			
-			local result = mysql_query(handler, "SELECT description, age, weight, height, skincolor FROM characters WHERE charactername='" .. getElementData(targetPlayer, "dbid") .. "'")
+			local result = mysql_query(handler, "SELECT description, age, weight, height, skincolor FROM characters WHERE id='" .. getElementData(targetPlayer, "dbid") .. "'")
 			local description = tostring(mysql_result(result, 1, 1))
 			local age = tostring(mysql_result(result, 1, 2))
 			local weight = tostring(mysql_result(result, 1, 3))
