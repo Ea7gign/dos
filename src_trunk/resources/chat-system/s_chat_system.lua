188a1c3b440bdea111905f34b25056c11d27f6d8
Mount <Mount@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-10-12 11:41:54 +0000

    removed /taxi
    git-svn-id: https://mabako.net:8081@282 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/chat-system/s_chat_system.lua
+++ src_trunk/resources/chat-system/s_chat_system.lua
@@ -62,16 +62,6 @@ function getPlayerName(p)
 	return gpn(p):gsub("_", " ")
 end
 
--- Taxi
-function taxi(thePlayer, commandName)
-	local logged = tonumber(getElementData(thePlayer, "loggedin"))
-	 
-	if (logged==1) then
-		outputChatBox("The new taxi number is /call 8294.", thePlayer, 255, 194, 15)
-	end
-end
-addCommandHandler("taxi", taxi)
-
 -- /ad
 function advertMessage(thePlayer, commandName, showNumber, ...)
 	local logged = tonumber(getElementData(thePlayer, "loggedin"))
