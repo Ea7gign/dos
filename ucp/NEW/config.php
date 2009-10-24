ead480a3dc7513e168dc27cfcf9317f989eefe7c
Fenix <Fenix@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-10-24 06:45:16 +0000

    Fixed a few UCP bugs
    git-svn-id: https://mabako.net:8081@416 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- ucp/NEW/config.php
+++ ucp/NEW/config.php
@@ -1,9 +1,9 @@
 <?php
 	$mysql_port = 3306;
-	$mysql_host = "67.210.235.106";
+	$mysql_host = "87.238.173.138";
 	//$mysql_user = "vgmtaucp";
 	//$mysql_pass = "gjsl49523nfk245";
 
-	$mysql_user = "phil";
-	$mysql_pass = "mta1884ac";
+	$mysql_user = "mtaucp";
+	$mysql_pass = "9sdf7as7f99sa7f";
 ?>
