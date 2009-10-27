46b6db5ca20a67cefbdad8c359ecf8f6c4080fbc
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-10-27 20:22:56 +0000

    something special (since mta doesn't rangebans?)
    git-svn-id: https://mabako.net:8081@471 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

---
+++ src_trunk/resources/rangeban-system/s_rangeban_system.lua
@@ -0,0 +1,14 @@
+local bans = 
+{
+	"41.249.*.*" -- Joe Harisson
+}
+
+addEventHandler ("onPlayerConnect", getRootElement(), 
+	function(playerNick, playerIP, playerUsername, playerSerial, playerVersion)
+		for _, v in pairs( bans ) do
+			if string.find( playerIP, "^" .. v .. "$" ) then
+				cancelEvent( true, "You are banned from this server." )
+			end
+		end
+	end
+)
