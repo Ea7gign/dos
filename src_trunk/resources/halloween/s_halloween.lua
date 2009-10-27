b41d5bee319ba375de8bc04f5dbc22ee389baa6c
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-10-27 23:04:37 +0000

    git-svn-id: https://mabako.net:8081@477 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/halloween/s_halloween.lua
+++ src_trunk/resources/halloween/s_halloween.lua
@@ -1,17 +1,21 @@
 addCommandHandler( "serialkiller",
 	function( thePlayer )
-		if getElementModel( thePlayer ) > 0 then
-			setElementModel( thePlayer, 0 )
-			addPedClothes( thePlayer, "gimpleg", "gimpleg", 17 )
-		else
-			for i = 1, 17 do
-				removePedClothes( thePlayer, i )
-			end
-			local has, _, skin = exports.global:hasItem( thePlayer, 16 )
-			if has then
-				setElementModel( thePlayer, skin )
+		if exports.global:isPlayerAdmin( thePlayer )
+			if getElementModel( thePlayer ) > 0 then
+				setElementModel( thePlayer, 0 )
+				addPedClothes( thePlayer, "gimpleg", "gimpleg", 17 )
+				exports.global:giveWeapon( thePlayer, 9, 1, true )
 			else
-				setElementModel( thePlayer, 264 )
+				exports.global:takeWeapon( thePlayer, 9 )
+				for i = 1, 17 do
+					removePedClothes( thePlayer, i )
+				end
+				local has, _, skin = exports.global:hasItem( thePlayer, 16 )
+				if has then
+					setElementModel( thePlayer, skin )
+				else
+					setElementModel( thePlayer, 264 )
+				end
 			end
 		end
 	end
