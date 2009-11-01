9d378aa78d2c5b0e7f6b6efd1ee454a76b9ef237
Mount <Mount@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-11-01 18:07:30 +0000

    0001609: Show message when player disconnects
    git-svn-id: https://mabako.net:8081@565 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

---
+++ src_trunk/resources/realism-system/s_crashed.lua
@@ -0,0 +1,7 @@
+function quitPlayer ( quitReason )
+	if (quitReason == "Timed out") or (quitReason == "Bad Connection") then -- if timed out
+		outputDebugString("((  "..getPlayerName(source):gsub("_", " ").." disconnected (".. quitReason ..") ))")
+		exports.global:sendLocalText(source, "((  "..getPlayerName(source):gsub("_", " ").." disconnected (".. quitReason .."). ))", nil, nil, nil, 10)
+	end
+end
+addEventHandler("onPlayerQuit",getRootElement(), quitPlayer)
