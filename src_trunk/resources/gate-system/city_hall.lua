0828a8a15c9f9594b5b5ba51af2be0ce7a0335e1
Mount <Mount@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-11-02 21:22:30 +0000

    Small change to lautner's gate
    git-svn-id: https://mabako.net:8081@588 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/gate-system/city_hall.lua
+++ src_trunk/resources/gate-system/city_hall.lua
@@ -10,7 +10,7 @@ local guard = createPed( 163, 370.7433166, 167.1261444, 1008.3828125 )
 setElementDimension( guard, 125 )
 setElementInterior( guard , 3 )
 setElementData( guard, "talk", 1 )
-setElementData( guard, "name", "Guard Clark" )
+setElementData( guard, "name", "Guard Jenkins" )
 
 local function useImpoundDoorb(thePlayer)
 	local team = getPlayerTeam(thePlayer)
@@ -20,12 +20,12 @@ local function useImpoundDoorb(thePlayer)
 
 		if (distance<=3) and (open==false) then
 			local gender = getElementData(thePlayer, "gender")
-			local genderm = "Mr."
+			local genderm = "Mister"
 			if (gender == 1) then
-				genderm = "Miss."
+				genderm = "Ma'am"
 			end
-			exports.global:sendLocalText(thePlayer, " *Guard Clark opens the door.", 255, 51, 102)
-			exports.global:sendLocalText(thePlayer, "Guard Clark says: Hello " .. genderm .. " " .. getPlayerName(thePlayer):gsub("_", " ") .. ". You can walk inside.", nil, nil, nil, 10)
+			exports.global:sendLocalText(thePlayer, " *Guard Jenkins opens the door.", 255, 51, 102)
+			exports.global:sendLocalText(thePlayer, "Guard Jenkins says: Hello " .. genderm .. " " .. getPlayerName(thePlayer):gsub("_", " ") .. ". You can walk inside/outside.", nil, nil, nil, 10)
 			open = true
 			moveObject(objGateb, 1000, 372.7575,166.7212,1008.582,0,0,90)
 			setTimer(closeImpoundDoorba, 5000, 1, thePlayer)
@@ -35,7 +35,7 @@ end
 addCommandHandler("gate", useImpoundDoorb)
 
 function closeImpoundDoorba(thePlayer)
-	exports.global:sendLocalText(thePlayer, " *Guard Clark closes the door.", 255, 51, 102)
+	exports.global:sendLocalText(thePlayer, " *Guard Jenkins closes the door.", 255, 51, 102)
 	moveObject(objGateb, 1000, 372.7575,166.7212,1008.582,0,0,-90)
 	setTimer(resetState1ba, 1000, 1)
 end
