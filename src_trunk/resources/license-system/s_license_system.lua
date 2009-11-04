789eb26c3e9c84c2bce676ab017aaf4186379c8e
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-11-04 17:32:22 +0000

    small pointer to /handbrake when you do the driving test
    git-svn-id: https://mabako.net:8081@609 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/license-system/s_license_system.lua
+++ src_trunk/resources/license-system/s_license_system.lua
@@ -141,7 +141,7 @@ function checkDMVCars(player, seat)
 	-- aka civilian previons
 	if getElementData(source, "owner") == -2 and getElementData(source, "faction") == -1 and getElementModel(source) == 436 then
 		if getElementData(player,"license.car") == 3 then
-			outputChatBox("(( You can use 'J' to start the engine ))", player, 0, 255, 0)
+			outputChatBox("(( You can use 'J' to start the engine and /handbrake to release the handbrake. ))", player, 0, 255, 0)
 		elseif seat > 0 then
 			outputChatBox("(( This DMV Car is for the Driving Test only. ))", player, 255, 194, 14)
 		else
