b04c37ffc9691fcc9f766c4055e5f8e0eb51a218
RayRay <RayRay@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-11-13 22:36:24 +0000

    Update- More weapons are backbone linked. (Shotgun, Sniper, Rifle, Spaz12)
    git-svn-id: https://mabako.net:8081@690 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/realism-system/c_weapons_back.lua
+++ src_trunk/resources/realism-system/c_weapons_back.lua
@@ -8,7 +8,7 @@ function weaponSwitch(prevSlot, newSlot)
 		weapons[source] = { }
 	end
 	
-	if (weapon == 30 or weapon == 31) and (isPedInVehicle(source)==false) then
+	if (weapon == 30 or weapon == 31 or weapon == 25 or weapon == 27 or weapon == 33 or weapon == 34) and (isPedInVehicle(source)==false) then
 		if (weapons[source][1] == nil or weapons[source][2] ~= weapon or weapons[source][3] ~= isPedDucked(source)) then -- Model never created
 			weapons[source][1] = createModel(source, weapon)
 			weapons[source][2] = weapon
@@ -18,7 +18,7 @@ function weaponSwitch(prevSlot, newSlot)
 			destroyElement(object)
 			weapons[source] = nil
 		end
-	elseif weapons[source] and weapons[source][1] and ( newWeapon == 30 or newWeapon == 31 or getPedTotalAmmo(source, 5) == 0 ) then
+	elseif weapons[source] and weapons[source][1] and ( newWeapon == 30 or newWeapon == 31 or newWeapon == 25 or newWeapon == 27 or newWeapon == 33 or newWeapon == 34 or getPedTotalAmmo(source, 5) == 0 ) then
 		local object = weapons[source][1]
 		destroyElement(object)
 		weapons[source] = nil
@@ -69,6 +69,14 @@ function createModel(player, weapon)
 		objectID = 356
 	elseif (weapon==30) then
 		objectID = 355
+	elseif (weapon==25) then
+		objectID = 349
+	elseif (weapon==27) then
+		objectID = 351
+	elseif (weapon==33) then
+		objectID = 357
+	elseif (weapon==34) then
+		objectID = 358
 	end
 	
 	local currobject = weapons[player][1]
