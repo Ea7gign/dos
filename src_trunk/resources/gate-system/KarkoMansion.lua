3f9cf93d928889dd3461d9c09ffaed1752f6ac25
Mount <Mount@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-11-25 14:17:46 +0000

    Changes to gate
    git-svn-id: https://mabako.net:8081@765 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/gate-system/KarkoMansion.lua
+++ src_trunk/resources/gate-system/KarkoMansion.lua
@@ -2,7 +2,7 @@ local Gate = {
 	[1] = createObject(988, -50.936084747314, -807.28820800781, 11.592994689941, 0, 0, 68.210021972656)
 }
 exports.pool:allocateElement(Gate[1])
-local GatePass = "karkosux"
+local GatePass = "megamilk"
 local open = false
 
 local function ResetOpenState()
@@ -15,7 +15,7 @@ local function closeDoor()
 end
 
 local function openDoor()
-	moveObject(Gate[1], 1000, -50.936084747314, -805.28820800781, 11.592994689941, 0, 0, 0)
+	moveObject(Gate[1], 1000, -48.921611785889, -802.21807861328, 11.592994689941, 0, 0, 0)
 	setTimer(closeDoor, 5000, 1)
 	open = true
 end
