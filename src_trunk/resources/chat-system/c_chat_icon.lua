67d81ae1adebb806cea21b6a74c29cd3527d2140
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-11-28 11:27:22 +0000

    update to chat bubbles
    git-svn-id: https://mabako.net:8081@782 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/chat-system/c_chat_icon.lua
+++ src_trunk/resources/chat-system/c_chat_icon.lua
@@ -12,6 +12,7 @@ setElementData(getLocalPlayer(), "chatting", 0, true)
 
 function render()
 	local x, y, z = getElementPosition(getLocalPlayer())
+	local reconx = getElementData(getLocalPlayer(), "reconx")
 	for key, value in ipairs(getElementsByType("player")) do
 		if (isElement(value)) and (isElementStreamedIn(value)) then
 			if (value~=getLocalPlayer()) then
@@ -21,14 +22,17 @@ function render()
 					local px, py, pz = getPedBonePosition(value, 6)
 					
 					local dist = getDistanceBetweenPoints3D(x, y, z, px, py, pz)
-					if (dist < 25) then
-						local reconning = getElementData(value, "reconx")
-						if (isLineOfSightClear(x, y, z, px, py, pz, true, false, false, false ) and isElementOnScreen(value)) and not (reconning) then
+					if (dist < 25 or reconx) and isElementOnScreen(value) and not getElementData(value, "reconx") and not getElementData(value, "freecam:state") then
+						local lx, ly, lz = getCameraMatrix()
+						local vehicle = getPedOccupiedVehicle(value)
+						local collision, cx, cy, cz, element = processLineOfSight(lx, ly, lz, x, y, z+1, true, true, true, true, false, false, true, false, vehicle)
+						if not (collision) or (reconx) then
 							local screenX, screenY = getScreenFromWorldPosition(px, py, pz+0.5)
 							if (screenX and screenY) then
 								dist = dist / 5
 									
 								if (dist<1) then dist = 1 end
+								if (dist>4 and reconx) then dist = 4 end
 								--if (dist>2) then dist = 2 end
 								local offset = 70 / dist
 								
