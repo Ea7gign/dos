21c9ae6a24320f05f464b035805e85f39b1c827c
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-12-21 00:47:31 +0000

    garage fix
    git-svn-id: https://mabako.net:8081@917 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/map-system/Fixes.lua
+++ src_trunk/resources/map-system/Fixes.lua
@@ -1,12 +1,12 @@
 _cx=createObject
 
-function createObjectX(m,x,y,z,a,b,c,l)
+function createObjectX(m,x,y,z,a,b,c,l,w)
 	local t=_cx(m,x,y,z,a,b,c)
 	if l then
 		setElementAlpha(t,l)
 	end
 	
-	local col = createColSphere(x,y,z,100)
+	local col = createColSphere(x,y,z,w or 100)
 	addEventHandler( "onClientColShapeHit", col,
 		function( element )
 			if element == getLocalPlayer( ) then
@@ -20,4 +20,4 @@ end
 
 createObjectX(2963,-791.77386474609,496.99490356445,1368.4364013672,0,0,269.17565917969,0) -- Marco's Bistro
 createObjectX(9093,681.64575195313,-447.3166809082,-26.669584274292,0,89.999969482422,0,0) -- Steakhouse
-createObjectX(8171,608.9248,-75.0812,1000.9953,0,270,0) -- Garage 111
+createObjectX(8171,608.9248,-75.0812,1000.9953,0,270,0,15) -- Garage 111
