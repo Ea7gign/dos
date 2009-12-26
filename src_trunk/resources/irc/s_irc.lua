f46ab5938b54029c811840f4fc36805673dc3dcb
VGMTAServer <VGMTAServer@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2009-12-26 14:57:38 +0000

    Changes to the distribute system of irc
    git-svn-id: https://mabako.net:8081@978 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/irc/s_irc.lua
+++ src_trunk/resources/irc/s_irc.lua
@@ -1,117 +1,54 @@
-server = "irc.multitheftauto.com"
-port = 6667
-username = "ValhallaGaming"
-username2 = "ValhallaGaming2"
-channel = "#Valhalla.echo"
-channeladmins = "#Valhalla.admins"
-pubchannel = "#mta"
-pubchannel2 = "#Valhalla"
-password = "adminmtavg"
+local server = "irc.gtanet.com"
+local port = 6667
+local username = "ValhallaGaming"
+local channeladmins = "#Valhalla.admins"
+local password = "adminmtavg"
 
-conn = nil
-conn2 = nil
-timer = nil
-useSecond = false
+local conn = { }
+local count = 0
+local distribute = 1
+local timer = nil
+
+function spawnBot()
+	local id = count+1
+	conn[id] = ircOpen(server, port, username .. id, channeladmins, password)
+	count = id
+end
 
 function initIRC()
 	ircInit()
-	conn = ircOpen(server, port, username, channel, password)
-
+	
+	spawnBot()
+	spawnBot()
+	spawnBot()
+	
 	sendMessage("Server Started.")
-	conn2 = ircOpen(server, port, username2, channel, password)
-	displayStatus()
-	timer = setTimer(displayStatus, 900000, 0)
-	--ircJoin(conn, pubchannel)
-	--ircJoin(conn2, pubchannel)
-	--ircJoin(conn, pubchannel2)
-	--ircJoin(conn2, pubchannel2)
-	ircJoin(conn, channeladmins)
-	ircJoin(conn2, channeladmins)
 end
 addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), initIRC)
 
 function stopIRC()
 	sendMessage("Server Stopped.")
-	ircPart(conn, channel)
-	ircPart(conn2, channel)
-	ircDisconnect(conn)
-	ircDisconnect(conn2)
-	killTimer(timer)
-	timer = nil
-	conn = nil
+
+	for id, connection in ipairs(conn) do
+		ircDisconnect(connection)
+	end
 end
 addEventHandler("onResourceStop", getResourceRootElement(getThisResource()), stopIRC)
 
 function sendMessage(message)
-
-	local time = getRealTime()
-	local hour = time.hour
-	local mins = time.minute
+	ircMessage(conn[distribute], channeladmins, tostring(message))
 	
-	hour = hour + 8
-	if (hour==24) then
-		hour = 0
-	elseif (hour>24) then
-		hour = hour - 24
-	end
-	
-	-- Fix time
-	if (hour<10) then
-		hour = 0 .. tostring(hour)
-	end
-	
-	if (mins<10) then
-		mins = 0 .. tostring(mins)
-	end
-	
-	outputDebugString(tostring(message))
-	
-	if not (useSecond) then
-		useSecond = true
-		ircMessage(conn, channeladmins, "[" .. hour .. ":" .. mins .. "] " .. tostring(message))
-	else
-		useSecond = false
-		ircMessage(conn2, channeladmins, "[" .. hour .. ":" .. mins .. "] " .. tostring(message))
+	distribute = distribute + 1
+	if (distribute > count) then
+		distribute = 1
 	end
 end
 
 function sendAdminMessage(message)
-	if not (useSecond) then
-		useSecond = true
-		ircMessage(conn, channeladmins, tostring(message))
-	else
-		useSecond = false
-		ircMessage(conn2, channeladmins, tostring(message))
+	ircMessage(conn[distribute], channeladmins, tostring(message))
+	
+	distribute = distribute + 1
+	if (distribute > count) then
+		distribute = 1
 	end
 end
-
---[[
-function displayStatus()
-	local playerCount = getPlayerCount()
-	local maxPlayers = getMaxPlayers()
-	local servername = getServerName()
-	local ip = "87.238.173.138:22003"
-		
-	local totalping = 0
-	local averageping = 0
-	
-	for key, value in ipairs(exports.pool:getPoolElementsByType("player")) do
-		totalping = totalping + getPlayerPing(value)
-	end
-	
-	if (playerCount>0) then
-		averageping = math.floor(totalping / playerCount)
-	end
-		
-	local output = servername .. " - " .. playerCount .. "/" .. maxPlayers .. "(" .. math.ceil((playerCount/maxPlayers)*100) .. "%) - " .. ip .. " - GameMode: Roleplay - Average Ping: " .. averageping .. "."
-	
-	if not (useSecond) then
-		useSecond = true
-		ircMessage(conn2, channeladmins, output)
-		ircMessage(conn2, pubchannel2, output)
-	else
-		useSecond = false
-		ircMessage(conn, channeladmins, output)
-		ircMessage(conn, pubchannel2, output)
-	end
-end]]
