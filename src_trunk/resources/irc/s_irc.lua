5297a5ec204f593856a784b3f9cf8a78369dad1f
VGMTAServer <VGMTAServer@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2010-01-10 11:04:06 +0000

    Updated the irc module
    git-svn-id: https://mabako.net:8081@1050 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/irc/s_irc.lua
+++ src_trunk/resources/irc/s_irc.lua
@@ -7,18 +7,25 @@ local password = "adminmtavgl337"
 local conn = { }
 local count = 0
 local distribute = 1
+local lastmessage = nil
+local lastmessagefrom = nil
 
-function spawnBot()
+function spawnBot(deaf)
 	local id = count+1
-	conn[id] = ircOpen(server, port, username .. id, channeladmins, password)
+	local nickname = username .. id
+	conn[id] = ircOpen(server, port, nickname, channeladmins, password)
 	count = id
+	
+	if (deaf) then
+		ircRaw(conn[id], "MODE "..nickname.." +d")
+	end
 end
 
 function initIRC()
 	ircInit()
-	spawnBot()
-	spawnBot()
-	spawnBot()
+	spawnBot(false)
+	spawnBot(true)
+	spawnBot(true)
 	
 	sendMessage("Server Started.")
 end
@@ -61,27 +68,41 @@ function sendRawCommand(command)
 end
 
 function irc_onPrivMsg( szChannel, szNick, szText )
-	-- filter our own messages
-	if string.find( szNick, username ) == 0 then
-		-- only react on the admin channel
+	if string.find( szNick, username ) == 1 then
+		-- do nothing
+	else
 		if szChannel == channeladmins then
-			-- asay, broadcast to /a
-			if string.find( szText, "!asay" ) == 1 then
-				local message =  string.sub( szText, 7 )
-				exports.logs:logMessage("[Admin Chat FROM IRC] " .. szNick .. ": " .. message, 3)
-				sendAdminMessage("[IRC Admin Chat] " .. szNick .. ": " .. message)
-				for k, arrayPlayer in ipairs(players) do
-					local logged = getElementData(arrayPlayer, "loggedin")
-					
-					if(exports.global:isPlayerAdmin(arrayPlayer)) and (logged==1) then
-						outputChatBox("IRC Admin " .. szNick .. ": " .. message, arrayPlayer, 51, 255, 102)
-					end
+			if (szNick == lastmessagefrom) and (szText == lastmessage) then
+				-- ignore the message
+			else
+				lastmessagefrom = szNick
+				lastmessage = szText
+				-- is it a command?
+				if string.sub( szText, 1, 1 ) == "!" then
+					processCommands( szChannel, szNick, szText )
 				end
-
-			-- dumbass function
-			elseif string.find( szText, "!players" ) == 1 then
-				ircMessage( pIRC, szChannel, "4There are currently " .. getPlayerCount() .. " players connected" )
 			end
 		end
+	end
+end
+
+function processCommands ( szChannel, szNick, szText )
+	-- asay, broadcast to /a
+	if string.find( szText, "!asay" ) == 1 then
+		outputDebugString ( source )
+		local message =  string.sub( szText, 7 )
+		exports.logs:logMessage("[Admin Chat FROM IRC] " .. szNick .. ": " .. message, 3)
+		sendAdminMessage("[IRC Admin Chat] " .. szNick .. ": " .. message)
+		local players = exports.pool:getPoolElementsByType("player")
+		for k, arrayPlayer in ipairs(players) do
+			local logged = getElementData(arrayPlayer, "loggedin")
+			
+			if(exports.global:isPlayerAdmin(arrayPlayer)) and (logged==1) then
+				outputChatBox("IRC Admin " .. szNick .. ": " .. message, arrayPlayer, 51, 255, 102)
+			end
+		end
+	-- dumbass function
+	elseif string.find( szText, "!players" ) == 1 then
+		sendAdminMessage("4There are currently " .. getPlayerCount() .. " players connected" )
 	end
 end
