2abed1f5080185887b71dc297b67d86fad29237e
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2010-01-17 23:13:56 +0000

    removed a debug output
    git-svn-id: https://mabako.net:8081@1083 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/realism-system/s_crashed.lua
+++ src_trunk/resources/realism-system/s_crashed.lua
@@ -1,6 +1,5 @@
 function quitPlayer ( quitReason )
 	if (quitReason == "Timed out") or (quitReason == "Bad Connection") then -- if timed out
-		outputDebugString("(( "..getPlayerName(source):gsub("_", " ").." disconnected (".. quitReason ..") ))")
 		exports.global:sendLocalText(source, "(( "..getPlayerName(source):gsub("_", " ").." disconnected (".. quitReason .."). ))", nil, nil, nil, 10)
 	end
 end
