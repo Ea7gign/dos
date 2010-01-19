4ef004c088f6a3348d8b7598e47e3e05198e42e4
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2010-01-19 12:24:28 +0000

    disable ped voices for invisible players
    git-svn-id: https://mabako.net:8081@1127 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

---
+++ src_trunk/resources/admin-system/Player/c_ped_voices.lua
@@ -0,0 +1,15 @@
+setTimer(
+	function()
+		for key, value in ipairs( getElementsByType( "player" ) ) do
+			if getElementAlpha( value ) < 127 and not getElementData( value, "ped:voice" ) then
+				setElementData( value, "ped:voice", { getPedVoice( value ) }, false )
+				setPedVoice( value, "PED_TYPE_DISABLED" )
+			elseif getElementData( value, "ped:voice" ) then
+				setPedVoice( value, unpack( getElementData( value, "ped:voice" ) ) )
+				setElementData( value, "ped:voice", false, false )
+			end
+		end
+	end,
+	1000,
+	0
+)
