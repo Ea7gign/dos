1b539cd51139c418be517345d51a44684cda6bf2
VGMTAServer <VGMTAServer@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2010-02-03 16:58:44 +0000

    Last tooltip update
    git-svn-id: https://mabako.net:8081@1245 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/tooltips-system/s_tooltips.lua
+++ src_trunk/resources/tooltips-system/s_tooltips.lua
@@ -4,7 +4,7 @@
 -- Vehicles
 addEventHandler("onVehicleEnter",getRootElement(),
 	function(thePlayer)
-		triggerClientEvent(thePlayer,"showHelp",getRootElement(),2)
+		triggerClientEvent(thePlayer,"tooltips:showHelp",getRootElement(),2)
 	end)
 
 -- Interiors
@@ -12,9 +12,9 @@ addEventHandler("onPickupHit",getRootElement(),
 	function(thePlayer)
 		if (getElementDimension(thePlayer)==0)then
 			if (getElementModel(source)==1318) then
-				triggerClientEvent(thePlayer,"showHelp",getRootElement(),4) -- Interiors
+				triggerClientEvent(thePlayer,"tooltips:showHelp",getRootElement(),4) -- Interiors
 			else
-				triggerClientEvent(thePlayer,"showHelp",getRootElement(),15) -- House buying & renting
+				triggerClientEvent(thePlayer,"tooltips:showHelp",getRootElement(),15) -- House buying & renting
 			end
 		end
 	end)
