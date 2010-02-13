4c37663a48cedb52a48276b3c230c04a4e9b96c5
mabako <mabako@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2010-02-13 00:30:52 +0000

    fixed voices thing
    git-svn-id: https://mabako.net:8081@1361 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/admin-system/Player/c_ped_voices.lua
+++ src_trunk/resources/admin-system/Player/c_ped_voices.lua
@@ -1,19 +1,27 @@
---[[setTimer(
+local voices = { }
+
+setTimer(
 	function()
 		for key, value in ipairs( getElementsByType( "player" ) ) do
 			if getElementDimension( value ) < 65000 then
-				if getElementAlpha( value ) < 127 and not getElementData( value, "ped:voice" ) then
-					setElementData( value, "ped:voice", { getPedVoice( value ) }, false )
+				if getElementAlpha( value ) < 127 and not voices[ value ] then
+					voices[ value ] = { getPedVoice( value ) }
 					setPedVoice( value, "PED_TYPE_DISABLED" )
-				elseif getElementData( value, "ped:voice" ) then
-					setPedVoice( value, unpack( getElementData( value, "ped:voice" ) ) )
-					setElementData( value, "ped:voice", false, false )
+				elseif voices[ value ] then
+					setPedVoice( value, unpack( voices[ value ] ) )
+					voices[ value ] = nil
 				end
-			elseif getElementData( value, "ped:voice" ) then
-				setElementData( value, "ped:voice", false, false )
+			elseif voices[ value ] then
+				voices[ value ] = nil
 			end
 		end
 	end,
 	1000,
 	0
-)]]--
+)
+
+addEventHandler( "onClientPlayerQuit", getRootElement( ),
+	function( )
+		voices[ source ] = nil
+	end
+)
