59967a59f0bfb4de7d35267f872cd00354b39cdc
VGMTAServer <VGMTAServer@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2010-02-14 20:35:02 +0000

    removed unneeded code
    git-svn-id: https://mabako.net:8081@1380 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/startup/s_startup.lua
+++ src_trunk/resources/startup/s_startup.lua
@@ -8,21 +8,4 @@ addEventHandler("onResourceStart", getResourceRootElement(), resStart)
 
 function loadGlobal()
 	restartResource(getResourceFromName("global"))
-	setTimer(displayCredits, 1000, 1)
-end
-
-function out(text)
-	local irc = getResourceFromName( "irc" )
-	if irc and getResourceState( irc ) == "running" then
-		exports.irc:sendMessage(text)
-	end
-	outputServerLog(text)
-end
-
-function displayCredits()
-	local ver = exports.global:getScriptVersion()
-	out("------------------------------------------------------------" .. ("-"):rep(#ver))
-	out("--  VG MTA:RP Script V" .. ver .. " Loaded - By vG.MTA Scripting Team  --")
-	out("--               www.valhallagaming.net                   " .. (" "):rep(#ver) .. "--")
-	out("------------------------------------------------------------" .. ("-"):rep(#ver))
 end
