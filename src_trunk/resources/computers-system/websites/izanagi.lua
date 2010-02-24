f7f0cab2d25d7bb3386513c477187ecc76c9ae9b
herbjr <herbjr@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2010-02-24 05:48:31 +0000

    added 2 websites: websites/gnocchi.lua with websites/images/gnocchi-banner.png
    update provided for websites/izanagi.lua:
    2010.02.20
    - updated 'My Works' website list
    - added a contact link on the image
    - added www.gnocchi.sa to my works
    - changed color scheme
    
    git-svn-id: https://mabako.net:8081@1444 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/computers-system/websites/izanagi.lua
+++ src_trunk/resources/computers-system/websites/izanagi.lua
@@ -3,17 +3,17 @@
 -------------------
 
 -- Website owner's forum name: Izanagi
--- Website owner's Character's name: Vincenzo_Rossi
+-- Website owner's Character's name: Ryuunosuke_Yasogami
 
 function www_izanagi_sa()
 	
 	local page_length = 396
-	guiSetText(internet_address_label, "Izanagi.sa - Get your website today!")
+	guiSetText(internet_address_label, "Izanagi.sa - Click on my logo to contact me")
 	guiSetText(address_bar,"www.izanagi.sa")
 	
 	-- Page Background Colour
 	-------------------------
-	bg = guiCreateStaticImage(0,0,660,page_length,"websites/colours/19.png",false,internet_pane)
+	bg = guiCreateStaticImage(0,0,660,page_length,"websites/colours/1.png",false,internet_pane)
 
 	local title_shadow = guiCreateLabel(161,24,350,64,"Izanagi",false,bg)
 	guiLabelSetHorizontalAlign(title_shadow,"center")
@@ -21,48 +21,39 @@ function www_izanagi_sa()
 	guiLabelSetColor(title_shadow,128,8,8)
 	
 	local title = guiCreateLabel(160,23,350,64,"Izanagi",false,bg)
-	guiLabelSetColor(title,  255,16,16)
+	guiLabelSetColor(title,  0,0,0)
 	guiLabelSetHorizontalAlign(title,"center")
 	guiSetFont(title,"sa-header")
 	
 	local head1 = guiCreateLabel(160,88,350,16, "My works",false,bg)
-	guiLabelSetColor(head1,  255,16,16)
+	guiLabelSetColor(head1,  0,0,0)
 	guiLabelSetHorizontalAlign(head1, "center")
 	guiSetFont(head1, "default-bold-small")
 	
-	local list1 = guiCreateLabel(160,104,350, 16, "Midoru Enterprises",false,bg)
-	guiLabelSetColor(list1, 255,255,255)
+	local list1 = guiCreateLabel(160,104,350, 16, "Gnocchi Ristorante",false,bg)
+	guiLabelSetColor(list1, 0,0,0)
 	guiLabelSetHorizontalAlign(list1, "center")
 	guiSetFont(list1, "default-normal")
 	
-	local list2 = guiCreateLabel(160,120,350,16, "www.midoruenterprises.sa",false,bg)
-	guiLabelSetColor(list2, 255,255,255)
+	local list2 = guiCreateLabel(160,120,350,16, "www.gnocchi.sa",false,bg)
+	guiLabelSetColor(list2, 0,0,0)
 	guiLabelSetHorizontalAlign(list2, "center")
 	guiSetFont(list2, "default-small")
 
 	local list3 = guiCreateLabel(160,152,350, 16, "Bibles",false,bg)
-	guiLabelSetColor(list3, 255,255,255)
+	guiLabelSetColor(list3, 0,0,0)
 	guiLabelSetHorizontalAlign(list3, "center")
 	guiSetFont(list3, "default-normal")
 	
 	local list4 = guiCreateLabel(160,168,350,16, "www.bibles.sa",false,bg)
-	guiLabelSetColor(list4, 255,255,255)
+	guiLabelSetColor(list4, 0,0,0)
 	guiLabelSetHorizontalAlign(list4, "center")
 	guiSetFont(list4, "default-small")
 	
-	local request_info = guiCreateLabel(8,300,230,96,"If you would like me to create you a website for a reasonable price, mail to izanagi@whiz.sa. I will try to sort and reply to your request ASAP.",false,bg)
-	guiLabelSetHorizontalAlign(request_info,"left",true)
-	guiLabelSetColor(request_info, 255,16,16)
-	
-	local logo = guiCreateStaticImage(519,286,146,110, "websites/images/izanagi-red.png", false, bg)
+	local logo = guiCreateStaticImage(519,286,146,110, "websites/images/izanagi-black.png", false, bg)
 
-	addEventHandler("onClientGUIClick",logo,function()
-		local url = tostring("www.izanagi.sa")
-		get_page(url)
-	end,false)
-	
 	addEventHandler("onClientGUIClick",list1,function()
-		local url = tostring("www.midoruenterprises.sa")
+		local url = tostring("www.gnocchi.sa")
 		get_page(url)
 	end,false)
 	
@@ -71,8 +62,12 @@ function www_izanagi_sa()
 		get_page(url)
 	end,false)
 	
-	
-	
+	addEventHandler( "onClientGUIClick", logo,
+	function( button )
+		if button == "left" then
+			compose_mail( "izanagi@whiz.sa" )
+		end
+	end, false)
 	----------------------------------------------- End of webpage design -- Do not edit below this line. -----------------------------------------------
 	
 	if(page_length>=397)then
@@ -83,3 +78,5 @@ function www_izanagi_sa()
 	end
 	
 end
+
+
