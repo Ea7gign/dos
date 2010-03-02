4ca09efbabbd861da28e920f0766732d07624973
Mount <Mount@8706e3ef-0d6a-4709-9ab6-f16f3f3d4656>
2010-03-02 08:09:07 +0000

    added mysql var to the apps-system
    git-svn-id: https://mabako.net:8081@1541 8706e3ef-0d6a-4709-9ab6-f16f3f3d4656

--- src_trunk/resources/apps-system/s_apps.lua
+++ src_trunk/resources/apps-system/s_apps.lua
@@ -1,5 +1,7 @@
+mysql = exports.mysql
+
 local function updateAppCount( )
-	local result = exports.mysql:query_fetch_assoc( "SELECT COUNT(*) AS count FROM accounts WHERE appstate = 1" )
+	local result = mysql:query_fetch_assoc( "SELECT COUNT(*) AS count FROM accounts WHERE appstate = 1" )
 	if result then
 		setElementData( getResourceRootElement( ), "openapps", tonumber( result.count ) )
 		return tonumber( result.count )
@@ -21,7 +23,7 @@ addEventHandler( "apps:show", getRootElement( ),
 	function( id )
 		if exports.global:isPlayerAdmin( source ) then
 			if id and tonumber( id ) then
-				local result = exports.mysql:query_fetch_assoc( "SELECT id, username, appgamingexperience, appcountry, applanguage, apphow, appwhy, appexpectations, appdefinitions, appfirstcharacter, appclarifications, appreason, appstate, adminnote, ( SELECT COUNT(*) FROM adminhistory WHERE id = " .. id .. ") AS history FROM accounts WHERE id = " .. id )
+				local result = mysql:query_fetch_assoc( "SELECT id, username, appgamingexperience, appcountry, applanguage, apphow, appwhy, appexpectations, appdefinitions, appfirstcharacter, appclarifications, appreason, appstate, adminnote, ( SELECT COUNT(*) FROM adminhistory WHERE id = " .. id .. ") AS history FROM accounts WHERE id = " .. id )
 				if result then
 					if tonumber( result.appstate ) == 1 then -- application is open
 						for key, value in pairs( result ) do
@@ -40,11 +42,11 @@ addEventHandler( "apps:show", getRootElement( ),
 			end
 			
 		
-			local result = exports.mysql:query( "SELECT id, username, appdatetime FROM accounts WHERE appstate = 1 ORDER BY appdatetime ASC" )
+			local result = mysql:query( "SELECT id, username, appdatetime FROM accounts WHERE appstate = 1 ORDER BY appdatetime ASC" )
 			if result then
 				local table = { }
 				while true do
-					local row = exports.mysql:fetch_assoc( result )
+					local row = mysql:fetch_assoc( result )
 					if row then
 						table[ #table + 1 ] = { tonumber( row.id ), row.username, row.appdatetime }
 					else
@@ -56,7 +58,7 @@ addEventHandler( "apps:show", getRootElement( ),
 				else
 					triggerClientEvent( source, "apps:showall", source, table )
 				end
-				exports.mysql:free_result( result )
+				mysql:free_result( result )
 			end
 		end
 	end
@@ -68,7 +70,7 @@ addEvent( "apps:update", true )
 addEventHandler( "apps:update", getRootElement( ),
 	function( account, state, reason )
 		if exports.global:isPlayerAdmin( source ) and ( state == 2 or state == 3 ) and tonumber( account.id ) then
-			exports.mysql:query_free( "UPDATE accounts SET appstate = " .. state .. ", appreason = '" .. exports.mysql:escape_string( reason ) .. "', apphandler = '" .. exports.mysql:escape_string( getElementData( source, "gameaccountusername" ) ) .. "', appdatetime=NOW() + INTERVAL 1 DAY WHERE id = " .. account.id .. " LIMIT 1" )
+			mysql:query_free( "UPDATE accounts SET appstate = " .. state .. ", appreason = '" .. mysql:escape_string( reason ) .. "', apphandler = '" .. mysql:escape_string( getElementData( source, "gameaccountusername" ) ) .. "', appdatetime=NOW() + INTERVAL 1 DAY WHERE id = " .. account.id .. " LIMIT 1" )
 			outputChatBox( "You have now " .. ( state == 3 and "accepted " or "denied " ) .. account.name .. "'s application.", source, 0, 255, 0 )
 			if updateAppCount( ) > 0 then
 				triggerEvent( "apps:show", source )
